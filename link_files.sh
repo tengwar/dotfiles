df01a79043352d01b6a351cb818376095c641ea5
Grzegorz G <tengwar@users.noreply.github.com>
2018-04-27 15:36:43 +0200

    Add a script for symlinking files
    Change the txt file with dotfile paths into a script for symlinking
    files to their destinations.

---
+++ link_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Where to symlink various dotfiles
+
+# gitconfig -> ~/.config/git/config
+# On macOS: ln -s ~/Code/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/Code/dotfiles/gitconfig ~/.config/git/config
+
+# vimrc -> ~/.vim/vimrc
+# If you set up Vim to support the XGD standard, use $XDG_CONFIG_HOME/vim/vimrc
+ln -s ~/Code/dotfiles/vimrc ~/.vim/vimrc
+
+# bash_profile -> ~/.bash_profile
+ln -s ~/Code/dotfiles/bash_profile ~/.bash_profile
+# bashrc -> ~/.bashrc
+ln -s ~/Code/dotfiles/bashrc ~/.bashrc
+# bash_private -> ~/.bash_private
+ln -s ~/Code/dotfiles/bash_private ~/.bash_private
